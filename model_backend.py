d1832f4c7ed6f63467b7221dd25e0624bc947a68
arne.lescrauwaet <arne.lescrauwaet@student.howest.be>
2022-11-12 21:46:32 +0100

    finish fit

--- model_backend.py
+++ model_backend.py
@@ -14,20 +14,27 @@ from models.experimental import attempt_load
 model.LABEL_STUDIO_ML_BACKEND_V2_DEFAULT = True
 
 IMG_DATA = './data/images/'
-LABEL_DATA = "./data/labels/"
-WEIGHTS = './fine_tuned_results/tiny-100epoch-bs8/yolov7-tiny-1OOepoch.pt'
+LABEL_DATA = './data/labels/'
+WEIGHTS = './config/checkpoints/starting_weights.pt'#save location for finetuned weights
+MODEL_PATH = './config/checkpoints/trained_weights.pt'#save location for weights after training
 DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 IMAGE_SIZE = (640,480)
 
 class BloodcellModel(LabelStudioMLBase):
     def __init__(self, weights=WEIGHTS,  device=DEVICE, img_size=IMAGE_SIZE, train_output=None, **kwargs):
         super(BloodcellModel, self).__init__(**kwargs)
-        self.device = device
         upload_dir = os.path.join(get_data_dir(), 'media', 'upload')
+        
+        if os.path.isfile(MODEL_PATH):
+            self.weights = MODEL_PATH
+        else:
+            self.weights = weights
+        
+        self.device = device
         self.image_dir = upload_dir
         self.img_size = img_size
         self.label_map = {}
-        self.model = attempt_load(weights, map_location=device)
+        self.model = attempt_load(self.weights, map_location=self.device)
         
         self.from_name, self.to_name, self.value, self.labels_in_config = get_single_tag_keys(
             self.parsed_label_config, 'RectangleLabels', 'Image'
@@ -46,15 +53,42 @@ class BloodcellModel(LabelStudioMLBase):
         image_url = task['data'][self.value]
         return image_url
 
-    def label2idx(self, label):
+    def label2idx(self,label):
+        #convert label to according index in data.yaml
         if label == 'Platelets':
             return 0
         if label == "RBC":
             return 1
         return 2
 
+    def move_files(self,files, percent, train_val="train/", img_label=IMG_DATA):
+        #move new img, label to train/val dir
+        if train_val=="train/":
+            start = 0
+            end = len(files)-percent 
+        else: 
+            start= len(files)-percent
+            end = len(files)
+
+        for file in files[start:end]:
+            shutil.move(os.path.join(img_label,file), os.path.join(img_label+train_val,file) )
+
+    def init_train_val(self):
+        #clean up left over training files
+        if len(os.listdir(IMG_DATA +"/train/")) >0:
+            shutil.rmtree(IMG_DATA+"/train/")
+            shutil.rmtree(IMG_DATA+"/val/")
+            shutil.rmtree(LABEL_DATA+"/train/")
+            shutil.rmtree(LABEL_DATA+"/val/")
+
+            os.makedirs(IMG_DATA+"/train/")
+            os.makedirs(IMG_DATA+"/val/")
+            os.makedirs(LABEL_DATA+"/train/")
+            os.makedirs(LABEL_DATA+"/val/")
+
     def fit(self, tasks, workdir=None, batch_size=16, num_epochs=10, **kwargs):
-        image_urls, image_labels = [], []
+        print("start training")
+        self.init_train_val()
         for task in tasks:
             
             if is_skipped(task):
@@ -63,39 +97,40 @@ class BloodcellModel(LabelStudioMLBase):
             image_url = self._get_image_url(task)
             image_path = self.get_local_path(image_url)
             image_name = image_path.split("\\")[-1]
+
             Image.open(image_path).save(IMG_DATA+image_name)
 
             for annotation in task['annotations']:
                 for bbox in annotation['result']:
-                    x = bbox['value']['x'] / self.img_size[0]
-                    y = bbox['value']['y'] / self.img_size[1]
-                    width = bbox['value']['width'] / self.img_size[0]
-                    height = bbox['value']['height'] / self.img_size[1]
+                    x_center = bbox['value']['x'] / 100
+                    y_center = bbox['value']['y'] / 100
+                    width = bbox['value']['width'] / 100
+                    height = bbox['value']['height'] / 100
                     label = bbox['value']['rectanglelabels']
                     label_idx = self.label2idx(label[0])
 
                     with open(LABEL_DATA+image_name[:-5]+'.txt', 'a') as f:
-                        f.write(f"{label_idx} {x} {y} {width} {height}\n")
+                        f.write(f"{label_idx} {x_center} {y_center} {width} {height}\n")
+        
+        img_files = os.listdir(IMG_DATA)[:-2]
+        val_percent = int(len(img_files)*0.3)
+        
+        self.move_files(img_files,val_percent)
+        self.move_files(img_files,val_percent,train_val="val/")
 
-        files = os.listdir(IMG_DATA)
-        no_of_files = int(len(files)*0.3)
+        label_files = os.listdir(LABEL_DATA)[:-2]
+        self.move_files(label_files,val_percent,img_label=LABEL_DATA)
+        self.move_files(label_files,val_percent,train_val="val/",img_label=LABEL_DATA)
 
-        for file_name in random.sample(files, no_of_files):
-            shutil.move(os.path.join(IMG_DATA, file_name[:-5]+'.jpeg'), IMG_DATA+"val/")
-            shutil.move(os.path.join(LABEL_DATA, file_name[:-5]+'.txt'), IMG_DATA+"val/")
+        os.system(f"python ./yolov7/train.py --workers 8 --device {self.device} --batch-size {batch_size} --data config/data.yaml --img 320 240 --cfg config/model_config.yaml \
+            --weights {self.weights} --name bloodcell_trained --hyp confid/hyp.scratch.p5.yaml --exist-ok")
 
-        for file in os.listdir(IMG_DATA):
-            shutil.move(os.path.join(IMG_DATA, file[:-5]+'.jpeg'), IMG_DATA+"train/")
-            shutil.move(os.path.join(IMG_DATA, file[:-5]+'.txt'), IMG_DATA+"train/")
+        #shutil.move(f"./runs/train/bloodcell_trained/best.pt", MODEL_PATH)#move trained weights to checkpoint folder
 
-
-            #TODO:
-            #change weight loading path to runs/name/weights/fine_tuned.pt
-
-        return {'model_path': "./", 'labels': ""}
+        return {'model_path': "./"}
     
     def predict(self, tasks, **kwargs):
-
+        print("start predictions")
         results = []
         all_scores= []
         print("LABELS IN CONFIG:",self.labels_in_config)
